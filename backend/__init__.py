33035cf6e155685b9bfde8da122a89cdc10931bf
Keenan Chan <keenanjchan@gmail.com>
2024-02-06 15:48:01 -0800

    set up skeleton react frontend

--- backend/__init__.py
+++ backend/__init__.py
@@ -9,13 +9,13 @@ from flask import (
     send_from_directory
 )
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 # use this when frontend setup!
-# app = Flask(
-#     __name__,
-#     static_folder='frontend/build',
-#     static_url_path=''
-# )
+app = Flask(
+    __name__,
+    static_folder='frontend/build',
+    static_url_path=''
+)
 
 from backend import routes
--- backend/routes.py
+++ backend/routes.py
@@ -6,18 +6,18 @@ from backend import app
 
 @app.route("/")
 def index():
-    return "Hello! I'm Keenan, welcome to my personal website."
+    # return "Hello! I'm Keenan, welcome to my personal website."
     
     # use when frontend setup!
-    # return send_from_directory(
-    #     app.static_folder,
-    #     'index.html'
-    # )
+    return send_from_directory(
+        app.static_folder,
+        'index.html'
+    )
 
 # TODO: use blueprints for API
 @app.route("/api/hello")
 def hello():
     return {
         'resultStatus': 'SUCCESS',
-        'message': '/api/hello worked!'
+        'message': '/api/hello! This is Keenan.'
     }
